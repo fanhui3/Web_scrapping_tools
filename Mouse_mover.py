f387d2729513b6eee3e63e979e07d30d76cc6944
johnny23@live.com.sg <johnny23@live.com.sg>
2023-02-12 14:07:55 +0800

    Created mouse moving app

--- Mouse_mover.py
+++ Mouse_mover.py
@@ -1,1 +1,10 @@
-import pyautogui
+import pyautogui as pg
+import time
+
+time.sleep(3)
+while True:
+    pg.move(800,0, duration=3)
+    pg.leftClick()
+    pg.move(-800,0, duration=3)
+    pg.leftClick()
+    time.sleep(5)
