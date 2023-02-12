b5eb3bc0fc8ea97406ada694586760a73d46b631
johnny23@live.com.sg <johnny23@live.com.sg>
2023-02-12 20:50:26 +0800

    threaded all module and slowed down tasks

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from stockrow_scraper_mouse_clicks import get_stockrow_data
-from subprocess import call
+from stockrow_scraper_api import get_stockrow_data
+from file_mover import format_copy_file
+import time
 
-ticker = "MMM"
-
-# def open_py_file():
-#     call(["python", "stockrow_scraper.py"])
+ticker = "fb"
 
 get_stockrow_data(ticker)
+time.sleep(3)
+format_copy_file()
